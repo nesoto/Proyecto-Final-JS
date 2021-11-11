a42d77c830cabeb4ff5b3303b4954455ee236e29
Nesoto <nsoto2020@inf.udec.cl>
2021-11-11 17:07:47 -0300

    Avance

--- js/script.js
+++ js/script.js
@@ -1,103 +1,43 @@
-
-let RESULTADO = 0;
-let cuantosNumeros;
-let numeros = [];
-let aux;
-
-const Usuario ={
-	nombre: "",
-	apellido: "",
-	edad: 0,
-}
+let eleccion;
+let recetas = [];
 
 
 
-Usuario.nombre = prompt("Hola! ¿Como te llamas?");
-Usuario.apellido = prompt("Hola "+ Usuario.nombre + " ¿Cual es tu apellido?");
-Usuario.edad = Number(prompt("¿Y tu edad?"));
+do{
+eleccion = Number(prompt(`¡Bienvenido! 
+¿Qué deseas hacer?:
+1.- Ingresar receta
+2.- Ver recetas
+3.- Salir`));
+}while(eleccion != 1 && eleccion != 2 && eleccion != 3);
 
-if (Usuario.edad >= 18){
-	alert("Bienvenido");
-}else{
-	alert("No puedes entrar, eres menor de edad.");
-}
-
-
-do {
-	respuesta = prompt(Usuario.nombre +" "+ Usuario.apellido+`
-	¿Te gustaría sumar o multiplicar?
-	-Sumar
-	-Multiplicar`);
-	aux = toLowerCase(respuesta);
-} while (aux != "sumar" && aux != "multiplicar");
-
-
-switch(aux){
-	case "sumar":
-		cuantosNumeros = Number(prompt("¿Cuantos numeros quieres sumar?"));
-		numeros = recibenumero(cuantosNumeros);
-		RESULTADO = sumar(numeros);
-		break;
-	case "multiplicar":
-		cuantosNumeros = Number(prompt("¿Cuantos numeros quieres multiplicar?"));
-		numeros = recibenumero(cuantosNumeros);
-		RESULTADO = multiplicar(numeros);
-		break;
-	default:
-		break;
-
-}
-
-alert("El resultado es: " + RESULTADO);
-
-alert("¡Gracias por utilizarme!");
-
-
-//////////////////////////////////Entregas Anteriores/////////////////////////////////////////////
-/*if (respuesta == "Si" || respuesta == "si"){
-	cuantosNumeros = Number(prompt("¿Cuantos numeros me vas a dar?"));
-	
-	numeros = recibenumero(cuantosNumeros);
-	RESULTADO = sumar(numeros);
-
-	alert("Gracias! El resultado es " + RESULTADO);
-}else if( respuesta == "No" || respuesta == "no"){
-	RESULTADO = 0;
-	alert("Esta bien :C");
-}*/
-
-///////////////////////////////////////////FUNCIONES/////////////////////////////////////////////
-
-function toLowerCase(string){
-	return string.toLowerCase();  
-}
-
-function sumar(numeros){
-	let resultado = 0;
-	for (let i=0; i<numeros.length; ++i){
-		resultado += numeros[i].valueOf();
-	}
-	return resultado;
-	
-}
-
-function multiplicar(numeros){
-	let resultado = numeros[0];
-	if (numeros.length > 1){
-		for (let i=1; i<numeros.length; ++i){
-			resultado *= numeros[i];
+switch(eleccion){
+	case 1:
+		let nombre = prompt("Ingresa el nombre de la receta");
+		let ctosingredientes = Number(prompt("Ingresa el número de ingredientes"));
+		let ingredientes = [];
+		for (let i=0; i<ctosingredientes; i++){
+			ingredientes[i] = "1.- "+(prompt("Ingresa el ingrediente N°"+(i+1)));
 		}
-		return resultado;
-	}else{
-		return resultado;
-	}
+		let ctospasos = Number(prompt("Ingresa el número de pasos"));
+		let pasos = [];
+		for (let i=0; i<ctospasos; i++){
+			pasos[i] = "Paso " + (i+1) +": "+(prompt("Ingresa el paso N°"+(i+1)));
+		}
+		recetas.push({Nombre: nombre, Ingredientes: ingredientes, Pasos: pasos});
+		break;
+	
+	case 2:
+		for (let i=0; i<recetas.length; i++){
+			console.log(`${i+1}. ${recetas[i].Nombre}`);
+		}
+		break;
+	
+	case 3:
+		console.log("¡Hasta luego!");
+		exit;
 }
 
-function recibenumero(cantidad){
-	for (let i=0; i<cantidad; ++i){
-		numeros[i] = Number(prompt("Dame un numero"));
-	}
-	return numeros;
-}
-
-
+sessionStorage.setItem("recetas", JSON.stringify(recetas));
+let recetas2 = JSON.parse(sessionStorage.getItem("recetas"));
+console.log(recetas2);

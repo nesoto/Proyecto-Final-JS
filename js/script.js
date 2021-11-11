86ba0a1d6cd9ee569ff66d80399540fbde03e0a2
Nesoto <nsoto2020@inf.udec.cl>
2021-11-11 19:56:36 -0300

    AVANCE

--- js/script.js
+++ js/script.js
@@ -23,7 +23,16 @@ eleccion = Number(prompt(`¡Bienvenido!
 
 Menu(eleccion);
 
-
+function Inicio(){
+	do{
+	eleccion = Number(prompt(`¡Bienvenido! 
+	¿Qué deseas hacer?:
+	1.- Ingresar receta
+	2.- Ver recetas
+	3.- Salir`));
+	}while(eleccion != 1 && eleccion != 2 && eleccion != 3);
+	Menu(eleccion);
+}
 
 function Menu(eleccion){
 
@@ -33,58 +42,39 @@ function Menu(eleccion){
 			let ctosingredientes = Number(prompt("Ingresa el número de ingredientes"));
 			let ingredientes = [];
 			for (let i=0; i<ctosingredientes; i++){
-				ingredientes[i] = `${i+1}.- `+(prompt("Ingresa el ingrediente N°"+(i+1)));
+				ingredientes[i] = ` ${i+1}.- `+(prompt("Ingresa el ingrediente N°"+(i+1)));
 			}
 			let ctospasos = Number(prompt("Ingresa el número de pasos"));
 			let pasos = [];
 			for (let i=0; i<ctospasos; i++){
-				pasos[i] = "Paso " + (i+1) +": "+(prompt("Ingresa el paso N°"+(i+1)));
+				pasos[i] = " Paso " + (i+1) +": "+(prompt("Ingresa el paso N°"+(i+1)));
 			}
 			let receta = new Receta(nombre, ingredientes, pasos);
 			recetas.push(receta);
+			Inicio();
 			break;
 		
 		case 2:
 			for (let i=0; i<recetas.length; i++){
-				console.log(`${i+1}. ${recetas[i].Nombre}`);
+				console.log(`
+				${i+1}. ${recetas[i].Nombre}
+				Ingredientes : 
+			   ${recetas[i].Ingredientes}
+				Pasos:
+			   ${recetas[i].Pasos}`);
 			}
+			Inicio();
 			break;
 		
 		case 3:
-			console.log("¡Hasta luego!");
-			exit;
+			alert("¡Hasta luego!");
+			return;
 	}
 }
-/*switch(eleccion){
-	case 1:
-		let nombre = prompt("Ingresa el nombre de la receta");
-		let ctosingredientes = Number(prompt("Ingresa el número de ingredientes"));
-		let ingredientes = [];
-		for (let i=0; i<ctosingredientes; i++){
-			ingredientes[i] = `${i+1}.- `+(prompt("Ingresa el ingrediente N°"+(i+1)));
-		}
-		let ctospasos = Number(prompt("Ingresa el número de pasos"));
-		let pasos = [];
-		for (let i=0; i<ctospasos; i++){
-			pasos[i] = "Paso " + (i+1) +": "+(prompt("Ingresa el paso N°"+(i+1)));
-		}
-		let receta = new Receta(nombre, ingredientes, pasos);
-		recetas.push(receta);
-		break;
-	
-	case 2:
-		for (let i=0; i<recetas.length; i++){
-			console.log(`${i+1}. ${recetas[i].Nombre}`);
-		}
-		break;
-	
-	case 3:
-		console.log("¡Hasta luego!");
-		exit;
-}*/
-sessionStorage.setItem("recetas", JSON.stringify(recetas));
+
+/*sessionStorage.setItem("recetas", JSON.stringify(recetas));
 let recetas2 = JSON.parse(sessionStorage.getItem("recetas"));
-console.log(recetas2);
+console.log(recetas2);*/
 
 
 /*  /////////////////IDEAS PARA EL MENU///////////////
